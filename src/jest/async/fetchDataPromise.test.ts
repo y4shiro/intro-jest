082275f313a6816cb1f3b2f4cbf3e3f26747c102
y4shiro <y4shironao@gmail.com>
2021-10-07 15:31:21 +0900

    Promise のテストを .resolves / .rejects で書き直した

--- src/jest/async/fetchDataPromise.test.ts
+++ src/jest/async/fetchDataPromise.test.ts
@@ -1,14 +1,9 @@
 import { fetchDataPromise, fetchDataPromiseReject } from './fetchDataPromise';
 
 test('the data is peanut butter', () => {
-  return fetchDataPromise().then((str) => {
-    expect(str).toBe('peanut butter');
-  });
+  return expect(fetchDataPromise()).resolves.toBe('peanut butter');
 });
 
 test('promise is rejected', () => {
-  expect.assertions(1);
-  return fetchDataPromiseReject().catch((str) => {
-    expect(str).toBe('error');
-  });
+  return expect(fetchDataPromiseReject()).rejects.toBe('error');
 });
